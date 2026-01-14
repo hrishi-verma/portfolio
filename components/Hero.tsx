e052da235db3a36476ec4d811b948d1d70d2364a
Hrishikesh Verma <rishi.verma50@gmail.com>
2026-01-13 19:23:35 -0700

    icon fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -112,12 +112,7 @@ export default function Hero() {
 
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className={`w-6 h-10 border-2 rounded-full flex justify-center ${theme === 'dark' ? 'border-gray-600' : 'border-gray-400'}`}>
-          <div className={`w-1 h-3 rounded-full mt-2 ${theme === 'dark' ? 'bg-gray-600' : 'bg-gray-400'}`} />
-        </div>
-      </div>
+
     </section>
   );
 }
